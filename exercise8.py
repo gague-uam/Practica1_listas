01ce771db3252b2307fc2d8f9909a8936fa4649f
gague-UAM <ggpaiz@uamv.edu.ni>
2025-03-29 16:27:50 -0600

    Lista ordenada alfabeticamente

---
+++ exercise8.py
@@ -0,0 +1,8 @@
+"""8. Escribe un programa que recorra una lista de cadenas y las ordene
+alfabéticamente en orden ascendente."""
+
+lista = ["Nayeon", "Jeongyeon", "Momo", "Sana", "Jihyo", "Mina", "Dahyun", "Chaeyoung", "Tzuyu"]
+print(lista)
+
+lista.sort()
+print(lista)
