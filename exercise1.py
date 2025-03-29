44df007e5b5a43ad0ca16e00fd7e6c6d218129e3
gague-UAM <ggpaiz@uamv.edu.ni>
2025-03-29 14:50:04 -0600

    Cadenas de una lista concatenadas

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,9 @@
 """Crea una lista de cadenas de texto. Escribe un programa que recorra esta
 lista y concatene todas las cadenas en una sola cadena, separadas por un
 espacio."""
+
+lista = ["TWICE", "Xdinary Heroes", "Stray Kids", "Day6", "New Jeans"]
+print(lista)
+
+espacio = " "
+print(espacio.join(lista))
